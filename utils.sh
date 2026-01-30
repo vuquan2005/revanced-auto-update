ef1382b0ebdcf3bc93e5ebaea5f85468016aa02f
Vũ Quân <66623851+vuquan2005@users.noreply.github.com>
2026-01-31 02:28:49 +0700

    Use github Secrets key

--- utils.sh
+++ utils.sh
@@ -459,8 +459,13 @@ get_archive_pkg_name() { echo "$__ARCHIVE_PKG_NAME__"; }
 
 patch_apk() {
 	local stock_input=$1 patched_apk=$2 patcher_args=$3 cli_jar=$4 patches_jar=$5
-	local cmd="env -u GITHUB_REPOSITORY java -jar $cli_jar patch $stock_input --purge -o $patched_apk -p $patches_jar --keystore=ks.keystore \
---keystore-entry-password=123456789 --keystore-password=123456789 --signer=jhc --keystore-entry-alias=jhc $patcher_args"
+	local ks_pass="${KEYSTORE_PASSWORD:-123456789}"
+	local key_pass="${KEY_PASSWORD:-123456789}"
+	local key_alias="${KEY_ALIAS:-jhc}"
+	local ks_path="${KEYSTORE_PATH:-ks.keystore}"
+
+	local cmd="env -u GITHUB_REPOSITORY java -jar $cli_jar patch $stock_input --purge -o $patched_apk -p $patches_jar --keystore=$ks_path \
+--keystore-entry-password=$key_pass --keystore-password=$ks_pass --signer=jhc --keystore-entry-alias=$key_alias $patcher_args"
 	if [ "$OS" = Android ]; then cmd+=" --custom-aapt2-binary=${AAPT2}"; fi
 	pr "$cmd"
 	if eval "$cmd"; then [ -f "$patched_apk" ]; else
